2fe52cbe25bd18f309938958c72e4de914febe71
bigred <rafalsobczak1989@gmail.com>
2020-07-06 15:11:16 +0200

    Initial Commit

---
+++ src/pl/carrental/model/client/BusinessClient.java
@@ -0,0 +1,46 @@
+package pl.carrental.model.client;
+
+import java.util.Objects;
+
+public class BusinessClient extends Client {
+    public static final String TYPE = "Klient biznesowy";
+
+    private String nip;
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public BusinessClient(String firstName, String lastName, String nip) {
+        super(firstName, lastName);
+        this.nip = nip;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + nip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BusinessClient that = (BusinessClient) o;
+        return Objects.equals(nip, that.nip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), nip);
+    }
+
+    @Override
+    public String toCsv() {
+        return (TYPE + ";") + getFirstName() + ";" + getLastName() + ";" + getNip();
+    }
+}
